9cf86ce8ea7571f8c8fed3adf9a705caf3b50ee9
rokobeo123 <Lamtu646@gmail.com>
2026-03-13 18:57:42 +0700

    add solution

---
+++ penguin/daynguyentocungnhau.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+#define fi first
+#define se second
+#define allr(x) x.rbegin(), x.rend();
+#define fasteio()                                                              \
+  ios::sync_with_stdio(false);                                                 \
+  cin.tie(nullptr);                                                            \
+  cout.tie(nullptr);
+void fre(const char *tenbai) {
+  string input_file = string(tenbai) + ".INP";
+  string output_file = string(tenbai) + ".OUT";
+  freopen(input_file.c_str(), "r", stdin);
+  freopen(output_file.c_str(), "w", stdout);
+}
+void uoc(){
+    
+}
+int main() {
+  fasteio();
+  ll n;
+  cin >> n;
+  ll a[n];
+  for (ll i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+
+  return 0;
+}
